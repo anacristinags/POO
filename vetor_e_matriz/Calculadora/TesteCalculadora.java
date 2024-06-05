6d49d8249303eff23f45ff5111188b4c4581ad8d
Ana Cristina Siqueira <145061688+anacristinags@users.noreply.github.com>
2024-06-05 20:46:23 -0300

    Create TesteCalculadora.java

---
+++ vetor_e_matriz/Calculadora/TesteCalculadora.java
@@ -0,0 +1,10 @@
+// EXERCICIO 17 - AULA 11
+
+public class TesteCalculadora {
+    public static void main(String[] args) {
+        Calculadora.somar(2,2);
+        Calculadora.subtrair(5,2);
+        Calculadora.multiplicar(5,5);
+        Calculadora.dividir(10,2);
+    }
+}
