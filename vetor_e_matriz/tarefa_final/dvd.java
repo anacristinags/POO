23243805b720bfb958682ae1b457991aee4c9ed8
Ana Cristina Siqueira <145061688+anacristinags@users.noreply.github.com>
2024-06-24 15:32:57 -0300

    Create dvd.java

---
+++ vetor_e_matriz/tarefa_final/dvd.java
@@ -0,0 +1,22 @@
+public class DVD extends Material{
+    int duracao;
+    public DVD(int id, String titulo, boolean disponivel, int duracao) {
+        super(id, titulo, disponivel);
+        this.duracao = duracao;
+    }
+    public int getDuracao() {
+        return duracao;
+    }
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+    @Override
+    public void listar(){
+        super.listar();
+        System.out.println("Duração: "+duracao);
+    }
+    @Override
+    public String toString() {
+        return super.toString() + "\nDuração: " + duracao;
+    }
+}
